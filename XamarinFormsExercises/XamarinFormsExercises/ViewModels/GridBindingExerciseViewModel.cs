40a4763347b1fef6037c4be1d5f99cf1c4355de7
Martin Lenart <martin_lenart@icloud.com>
2022-04-13 14:07:36 +0200

    updated

---
+++ XamarinFormsExercises/XamarinFormsExercises/ViewModels/GridBindingExerciseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinFormsExercises.ViewModels
+{
+	public class GridBindingExerciseViewModel
+	{
+		public string Name { get; set; } = "Bombay";
+		public string Email { get; set; } = "Bombay@bombay.com";
+
+	}
+
+}
---
+++ XamarinFormsExercises/XamarinFormsExercises/Views/NonTrivialBinding/GridBindingExercise.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using XamarinFormsExercises.ViewModels;
+
+namespace XamarinFormsExercises.Views.NonTrivialBinding
+{
+	public partial class GridBindingExercise : ContentPage
+	{
+		public GridBindingExercise()
+		{
+			InitializeComponent();
+			BindingContext = new GridBindingExerciseViewModel();
+		}
+	}
+}
+
